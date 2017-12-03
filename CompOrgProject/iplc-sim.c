bde993c569f426b745b7940f4062e8cb40968a85
Achau9 <achau@afsenyc.org>
2017-12-03 12:42:01 -0500

    Made changes to the lw,sw,and nop function

--- CompOrgProject/iplc-sim.c
+++ CompOrgProject/iplc-sim.c
@@ -335,11 +335,30 @@ void iplc_sim_process_pipeline_rtype(char *instruction, int dest_reg, int reg1, 
 void iplc_sim_process_pipeline_lw(int dest_reg, int base_reg, unsigned int data_address)
 {
     /* You must implement this function */
+    iplc_sim_push_pipeline_stage();
+    //itype defined at enum
+    pipelineline[FETCH].itype = LW;
+    pipeline[FETCH].instruction_address = instruction_address;
+    //Doing .lw to store each variable defined in the typedef and the arguments
+    pipeline[FETCH].stage.lw.dest_reg=dest_reg;
+    pipeline[FETCH].stage.lw.base_reg=base_reg;
+    pipeline[FETCH].stage.lw.data_address=data_address;
+
+
 }
 
 void iplc_sim_process_pipeline_sw(int src_reg, int base_reg, unsigned int data_address)
 {
     /* You must implement this function */
+    iplc_sim_push_pipeline_stage();
+    //itype defined at enum
+    pipelineline[FETCH].itype = SW;
+    pipeline[FETCH].instruction_address = instruction_address;
+    //Doing .sw to store each variable defined in the typedef and the arguments
+    pipeline[FETCH].stage.sw.src_reg=src_reg;
+    pipeline[FETCH].stage.sw.base_reg=base_reg;
+    pipeline[FETCH].stage.sw.data_address=data_address;
+
 }
 
 void iplc_sim_process_pipeline_branch(int reg1, int reg2)
@@ -360,6 +379,10 @@ void iplc_sim_process_pipeline_syscall()
 void iplc_sim_process_pipeline_nop()
 {
     /* You must implement this function */
+    iplc_sim_push_pipeline_stage();
+    //itype defined at enum
+    pipelineline[FETCH].itype = NOP;
+    pipeline[FETCH].instruction_address = instruction_address;
 }
 
 /************************************************************************************************/
